4af1ed81a1b4abc62f609a4cb299ff58000d2908
Y-Yoshimoto <mewyoshimoto@gmail.com>
2019-09-04 07:38:28 +0900

    tmp

--- ZabbixServerAPITest/NodeJS/auth/auth.js
+++ ZabbixServerAPITest/NodeJS/auth/auth.js
@@ -5,12 +5,14 @@ console.log(auth.id);
 
 ////リクエスト生成
 var request = require('request');
+var jar = request.jar();
 var options = {
     url: 'http://192.168.1.116/api_jsonrpc.php',
     method: 'POST',
     headers: {'Content-Type':'application/json'},
     json: true,
-    json: auth
+    json: auth,
+    jar: jar
 }
     console.log(options);
 
@@ -20,6 +22,7 @@ request(options, function (error, response, body) {
     //console.log(response);
     console.log(body);
     console.log(body.result);
+    console.log(jar);
     //レスポンスJSONファイル出力
     var fs = require('fs');
     fs.writeFileSync('./authResult.json', JSON.stringify(body, null, '    '));
--- ZabbixServerAPITest/NodeJS/auth/getGraph.js
+++ ZabbixServerAPITest/NodeJS/auth/getGraph.js
@@ -4,6 +4,10 @@ console.log(authResult.result);
 
 var request = require('request');
 var jar = request.jar();
+var ID = 'zbx_sessionid=' + authResult.result;
+var cookie = request.cookie(ID);
+console.log(cookie);
+jar.setCookie(cookie, 'http://192.168.1.116/');
 console.log(jar);
 
 ////リクエスト生成
@@ -11,7 +15,7 @@ var request = require('request');
 var options = {
     url: 'http://192.168.1.116/chart2.php?graphid=524',
     method: 'GET',
-    headers: {'Content-Type':'application/json'},
+    encoding: null,
     jar: jar
 }
     console.log(options);
@@ -23,5 +27,5 @@ request(options, function (error, response, body) {
     console.log(body);
     console.log(jar);
     var fs = require('fs');
-    fs.writeFileSync('./graph.png',body);
+    fs.writeFileSync('./graph.png', body, 'binary');
 })
